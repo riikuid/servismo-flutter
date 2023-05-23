fab6d100a6a42164ba35aa7c7ac6edc84ca9bf44
riikuid <fahmibambe@gmail.com>
2023-05-23 08:44:56 +0700

    banner link

--- lib/widgets/banner_card_reversed.dart
+++ lib/widgets/banner_card_reversed.dart
@@ -30,68 +30,37 @@ class BannerCardReversed extends StatelessWidget {
       child: Container(
         padding: EdgeInsets.symmetric(horizontal: 25, vertical: 12),
         margin: EdgeInsets.only(
-          top: defaultMargin,
+          top: 10,
           right: defaultMargin,
           left: defaultMargin,
         ),
-        height: 200,
+        height: 80,
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10), color: alertColor),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
+            borderRadius: BorderRadius.circular(10),
+            color: Color(0xFFB65FCF).withOpacity(0.7)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  AutoSizeText(
-                    product.description,
-                    style:
-                        whiteTextStyle.copyWith(fontSize: 20, fontWeight: bold),
-                    overflow: TextOverflow.clip,
-                    minFontSize: 10,
-                  ),
-                  // Text(
-                  //   "Kendalamu",
-                  //   style:
-                  //   whiteTextStyle.copyWith(fontSize: 20, fontWeight: bold),
-                  // ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Isi Sekarang",
-                        style: whiteTextStyle.copyWith(
-                            fontWeight: medium, fontSize: 14),
-                      ),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Icon(
-                        Icons.arrow_forward_rounded,
-                        color: whiteColor,
-                        size: 18,
-                      )
-                    ],
-                  )
-                ],
-              ),
+            AutoSizeText(
+              product.description,
+              style: whiteTextStyle.copyWith(fontSize: 20, fontWeight: bold),
+              overflow: TextOverflow.clip,
+              minFontSize: 10,
             ),
-            Expanded(
-              child: AspectRatio(
-                aspectRatio: 1 / 1,
-                child: Image.asset(
-                  'assets/image_servis_problem.png',
-                  // height: 120,
-                  // fit: BoxFit.fitWidth,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "Isi Sekarang",
+                  style:
+                      whiteTextStyle.copyWith(fontWeight: medium, fontSize: 14),
                 ),
-              ),
+                SizedBox(
+                  width: 20,
+                ),
+              ],
             )
           ],
         ),
